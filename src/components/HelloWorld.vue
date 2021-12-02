6db1e99b78130da17466831450872ebf33029b91
Evgenii <eskov@cashalot.co>
2021-12-02 16:34:53 +1000

    build 1 page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,129 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <div class="input_data">
+      <h1
+        :class="this.GET_LAST_USER_ID() && this.isActive ? 'notification' : 'non_active'"
+      >
+        Восстановленны данные с последнего посещения. id пользователя: {{GET_LAST_USER_ID()}}
+      </h1>
+      <p class="lab">Введите id пользователя, чтобы загрузить его посты:</p>
+      <p class="note">*Доступные пользователь с номером 1 - 10</p>
+      <input v-model="this.userId" type="number">
+      <button
+        @click="fetchPosts(this.userId)"
+      >
+        Запросить!
+      </button>
+    </div>
+
+
+    <div v-if="this.isLoading" class="prealoder">
+      <h1>Loading...</h1>
+    </div>
+    <div v-else class="conteiner">
+      <div
+          class="item"
+          v-for="post in this.posts"
+      >
+        <p>{{post.title}}</p>
+        <p>{{post.body}}</p>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import {mapGetters, mapMutations} from "vuex";
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      isLoading: false,
+      isActive: true,
+      userId: '',
+      posts : []
+    }
+  },
+  methods:{
+    ...mapGetters(["GET_LAST_USER_ID"]),
+    ...mapMutations(["SAVE_LAST_USER_ID"]),
+    async fetchPosts(id){
+      if(!id) {
+        alert('Введите id пользователя!')
+      } else {
+        this.isLoading = true;
+        this.SAVE_LAST_USER_ID(id);
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`);
+        const data = await res.json();
+        this.posts = data;
+        this.isLoading = false;
+      }
+
+    }
+  },
+  created() {
+    this.fetchPosts(this.GET_LAST_USER_ID())
+  },
+  mounted(){
+    setTimeout( () => {
+      this.isActive = false
+    }, 2000)
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hello{
+  width: 600px;
+  margin: 0 auto;
+}
+.lab{
+  font-size: 16px;
+  font-weight: 400;
+  margin: 10px 0;
+  padding: 0;
+}
+.notification{
+  opacity: 1;
+  visibility: visible;
+  width: 500px;
+  height: 150px;
+  margin: 0 auto 30px auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #000;
+  border-radius: 10px;
+  font-size: 24px;
+  font-weight: 300;
+  background-color: #f7fffe;
+}
+.non_active{
+  opacity: 0;
+  visibility: hidden;
+  height: 0;
+}
+.note{
+  font-size: 14px;
+  font-weight: 300;
+  margin: 0;
+  padding: 0;
+}
+.item{
+  width: 500px;
+  margin: 20px auto;
+  background-color: #ebf1ff;
+  padding: 10px;
+  text-align: left;
+  border-radius: 10px;
+
+}
 h3 {
   margin: 40px 0 0;
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import createPersistedState from "vuex-persistedstate";
+
 
 export default createStore({
   state: {
+    lastUserId: undefined
   },
   mutations: {
+    SAVE_LAST_USER_ID(state, id){
+      state.lastUserId = id;
+    }
   },
   actions: {
   },
+  getters:{
+    GET_LAST_USER_ID(state){
+      return state.lastUserId;
+    }
+  },
   modules: {
-  }
+  },
+  plugins: [createPersistedState()]
 })
