e174e1b3b086456378bf6868ddcd73b4b7a9f708
Evgenii <eskov@cashalot.co>
2021-12-05 17:10:22 +1000

    .

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,16 +1,14 @@
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-import { registerRoute } from 'workbox-routing';
-
 // registerRoute(
 //     ({url}) => url.pathname.startsWith(`/users/${id}/posts`),
 //     new CacheFirst()
 // )
 
 console.log('Hello from service-worker.js');
-// workbox.routing.registerRoute()
-registerRoute(
+
+workbox.routing.registerRoute(
     new RegExp('\.(png|svg|jpg|jpeg)$'),
     workbox.strategies.cacheFirst({
         cacheName: 'My-cache-Images',
